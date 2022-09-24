80a57ec53550f867781bc571ef56afa4cac16ed1
Mouelhi Mohamed Fakhreddine <m.mouelhi0603@myu.universitecentrale.tn>
2022-09-25 00:53:55 +0100

    Developed getBranches method in BranchesService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BranchComponent } from './components/branch/branch.component';
+import { BranchesComponent } from './components/branches/branches.component';
 
 @NgModule({
-  declarations: [AppComponent, BranchComponent],
+  declarations: [AppComponent, BranchComponent, BranchesComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
