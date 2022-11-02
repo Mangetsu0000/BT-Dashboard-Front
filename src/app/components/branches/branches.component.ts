954bab8d63ef143f2036ef0a0659169fc774419e
Mouelhi Mohamed Fakhreddine <m.mouelhi0603@myu.universitecentrale.tn>
2022-11-02 16:56:47 +0100

    Developed CRUD operations

--- src/app/components/branches/branches.component.ts
+++ src/app/components/branches/branches.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Branch } from 'src/app/models/branch';
+import { BranchToEdit } from 'src/app/models/branch-to-edit';
 import { BranchesService } from 'src/app/services/branches.service';
 
 @Component({
@@ -11,13 +12,13 @@ export class BranchesComponent implements OnInit {
   searchTerm!: any;
   branches: Branch[] = [];
   creating: boolean = false;
+  editing: boolean = false;
+  branchToEdit!: BranchToEdit;
+  buttonText!: string;
   constructor(private branchesService: BranchesService) {}
 
   ngOnInit(): void {
-    this.branchesService.getBranches().subscribe((data) => {
-      this.branches = data;
-      console.log(data);
-    });
+    this.getData();
   }
   search() {
     if (this.searchTerm) {
@@ -33,10 +34,47 @@ export class BranchesComponent implements OnInit {
       });
     }
   }
+  getData() {
+    this.branchesService.getBranches().subscribe((data) => {
+      this.branches = data;
+      return data;
+    });
+  }
   onToggleCreate() {
+    this.buttonText = 'Create';
     this.creating = !this.creating;
   }
-  onFormSubmit() {
-    this.creating = false;
+  onFormSubmit(form: any) {
+    if (this.creating) {
+      this.branchesService.createBranch(form).subscribe((res) => {
+        this.branches.push(res);
+        this.creating = false;
+      });
+    } else if (this.editing) {
+      this.buttonText = 'Edit';
+      this.branchesService
+        .editBranch(this.branchToEdit.id!, form)
+        .subscribe(() => {
+          this.branchesService.getBranches().subscribe(() => {
+            this.branches.sort((a, b) => a.id! - b.id!);
+            this.editing = false;
+          });
+        });
+    }
+  }
+  onDelete(branchId: number) {
+    this.branchesService.deleteBranch(branchId).subscribe(() => {
+      this.getData();
+    });
+  }
+  onEdit(branch: BranchToEdit) {
+    this.branchToEdit = branch;
+    console.log(this.branchToEdit.name);
+    this.editing = true;
+    this.buttonText = 'Update';
+  }
+  onFormCancel(status: boolean) {
+    this.creating = status;
+    this.editing = status;
   }
 }
